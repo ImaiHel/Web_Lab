0723b48ae72a27aa3e48114e0a127dcef076e4fe
ImaiHel <ren19375@gmail.com>
2026-08-04 19:32:42 +0300

    Update index.js

--- Lab_9/index.js
+++ Lab_9/index.js
@@ -1,39 +1,23 @@
-let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+let drums = document.querySelectorAll(".drum");
 
-for (let i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        let buttonInnerHTML = this.innerHTML;
-        let audio;
-
-        switch (buttonInnerHTML) {
-            case "w":
-                audio = new Audio("sounds/tom-1.mp3");
-                break;
-            case "a":
-                audio = new Audio("sounds/tom-2.mp3");
-                break;
-            case "s":
-                audio = new Audio("sounds/tom-3.mp3");
-                break;
-            case "d":
-                audio = new Audio("sounds/tom-4.mp3");
-                break;
-            case "j":
-                audio = new Audio("sounds/snare.mp3");
-                break;
-            case "k":
-                audio = new Audio("sounds/crash.mp3");
-                break;
-            case "l":
-                audio = new Audio("sounds/kick-bass.mp3");
-                break;
-            default:
-                console.log(buttonInnerHTML);
-                return;
-        }
-
-        audio.play().catch(function(error) {
-            console.log("Ошибка воспроизведения:", error);
-        });
+for (let i = 0; i < drums.length; i++) {
+    drums[i].addEventListener("click", function() {
+        let button = this.innerHTML;
+        makeSound(button);
     });
+}
+
+function makeSound(key) {
+    let audio;
+    switch (key) {
+        case "w": audio = new Audio("sounds/tom-1.mp3"); break;
+        case "a": audio = new Audio("sounds/tom-2.mp3"); break;
+        case "s": audio = new Audio("sounds/tom-3.mp3"); break;
+        case "d": audio = new Audio("sounds/tom-4.mp3"); break;
+        case "j": audio = new Audio("sounds/snare.mp3"); break;
+        case "k": audio = new Audio("sounds/crash.mp3"); break;
+        case "l": audio = new Audio("sounds/kick-bass.mp3"); break;
+        default: return;
+    }
+    audio.play();
 }
